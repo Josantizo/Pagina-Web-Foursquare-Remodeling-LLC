640d799f6ca6fa24ac27ed89f13f79860ed8ad05
morataya5 <diegojaviermorataya@gmail.com>
2025-07-06 11:19:34 -0600

    Creación de proyecto, de página principal y 6 secciones para la página, cambios en el archivo .env para base de datos y funcionalidad de la página

--- Proyecto Constructora/routes/web.php
+++ Proyecto Constructora/routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+
+Route::get('/handyman', function () {
+    return view('handyman');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
